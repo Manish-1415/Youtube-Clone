f0183a417c524247b92dc864ed959416674ab9cb
Manish Shinde <manishshinde1415@gmail.com>
2025-12-31 14:32:44 +0530

    Refresh Token Validation Middleware added.

---
+++ backend/src/middlewares/verifyRefreshToken.middleware.js
@@ -0,0 +1,18 @@
+import ApiError from "../utility/ApiError";
+import { verifyRefreshToken } from "../utility/tokens";
+
+const validateRefreshToken = (req , res , next) => {
+    try {
+        if(!req.cookies.refreshToken) throw new ApiError(400 , "Please Provide Valid Refresh Token");
+
+        const refreshTokenPayload = verifyRefreshToken(req.cookies.refreshToken);
+
+        req.refToken = refreshTokenPayload;
+
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
+
+export default validateRefreshToken;
