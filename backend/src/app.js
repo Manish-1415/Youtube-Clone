64590f0d67ce2036e51a3a13125ad52e5a315724
Manish Shinde <manishshinde1415@gmail.com>
2026-01-03 11:04:46 +0530

    Added Routers path in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ const app = express();
 import cookieParser from "cookie-parser";
 import cors from "cors"
 
+// useful middlewares
 
 app.use(express.json());
 
@@ -15,5 +16,28 @@ app.use(express.urlencoded( {extended : true} ));
 app.use(cookieParser())
 
 app.use(cors({origin : process.env.CORS_ORIGIN , credentials : true}));
+
+
+
+
+// Routers
+import authRouter from "./modules/auth/auth.route.js";
+import userRouter from "./modules/user/user.route.js";
+import channelRouter from "./modules/channel/channel.route.js";
+import videoRouter from "./modules/video/video.route.js";
+import subscriptionRouter from "./modules/subscription/subscription.route.js";
+import commentRouter from "./modules/comments/comment.route.js";
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/channels", channelRouter);
+app.use("/api/v1/videos" , videoRouter);
+app.use("/api/v1/subscriptions" , subscriptionRouter);
+app.use("/api/v1/comments" , commentRouter);
+
+// err middleware at last
+import errorMiddleware from "./middlewares/error.middleware.js";
+
+app.use(errorMiddleware());
 
 export default app;
