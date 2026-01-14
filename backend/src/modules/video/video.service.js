b1c46ac50307085d027b13b6b20dffd2ff87d9ba
Manish Shinde <manishshinde1415@gmail.com>
2026-01-14 12:33:47 +0530

    Cloudinary file delete functionality added for Video module.

--- backend/src/modules/video/video.service.js
+++ backend/src/modules/video/video.service.js
@@ -1,3 +1,4 @@
+import eventBus from "../../eventBus/index.js";
 import ApiError from "../../utility/ApiError"
 import { Channel } from "../channel/channel.model.js";
 import {Video} from "./video.model.js"
@@ -23,11 +24,25 @@ const videoService = {
 
         if(!findVideo) throw new ApiError(404 , "Video Not Found");
 
-        // if video is there then update it.
+        let oldThumbnailPublicId ;
+        let oldVideoPublicId;
 
+        if(videoInfoObj.video.path && videoInfoObj.thumbnail.path && findVideo.video.path && findVideo.thumbnail.path) {
+            oldThumbnailPublicId = findVideo.thumbnail.path;
+            oldVideoPublicId =  findVideo.video.path;
+        }
+
+        // if video is there then update it.
         const findAndUpdateTheVid = await Video.findByIdAndUpdate(videoId , videoInfoObj ,{new : true});
 
         if(!findAndUpdateTheVid) throw new ApiError(500 , "Error Occurred While Saving DB Entry");
+
+        if(oldThumbnailPublicId && oldVideoPublicId) {
+            eventBus.emit("CLOUDINARY_THUMB_VIDEO_DELETE", {
+            thumbnailPublicId : oldThumbnailPublicId,
+            videoPublicId : oldVideoPublicId
+        })
+        }
 
         return findAndUpdateTheVid;
     },
@@ -38,9 +53,23 @@ const videoService = {
 
         if(!findVideo) throw new ApiError(404 , "Video Not Found , for deleting");
 
+        let oldThumbnailPublicId ;
+        let oldVideoPublicId ;
+        if(findVideo.video.path && findVideo.thumbnail.path) {
+            oldThumbnailPublicId = findVideo.thumbnail.path;
+            oldVideoPublicId = findVideo.video.path;
+        }
+
         const findAndDeleteVideo = await Video.findByIdAndDelete(findVideo);
 
         if(!findAndDeleteVideo) throw new ApiError(500 , "Error Occurred while deleting Video");
+
+        if(oldThumbnailPublicId && oldVideoPublicId) {
+            eventBus.emit("CLOUDINARY_THUMB_VIDEO_DELETE" , {
+                videoPublicId : oldVideoPublicId,
+                thumbnailPublicId : oldThumbnailPublicId
+            });
+        }
 
         return findAndDeleteVideo;
     },
